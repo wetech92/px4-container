8e785ad8e46befff9f258d28e9aa02a97717ce05
merlin <samsani@naver.com>
2022-07-20 20:37:48 +0900

    Convenience script for pkg init and update

---
+++ scripts/initRepo.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Initialize and update package repo
+apt update
+apt install -y software-properties-common
+add-apt-repository universe
+apt update
+apt install -y curl gnupg lsb-release
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu \
+    $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+apt-get update
