c843a971d880ab9d14c424680038ad3b274d1cfa
merlin <samsani@naver.com>
2022-07-26 10:57:17 +0900

    Convenience script for building uRTPS bridge again

---
+++ scripts/rebuildPX4ros.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+rm -rf /root/px4_ros/src/px4_msgs/msg/*
+
+python3 /root/PX4-Autopilot/msg/tools/uorb_to_ros_urtps_topics.py \
+    -i /root/PX4-Autopilot/msg/tools/urtps_bridge_topics.yaml \
+    -o /root/px4_ros/src/px4_ros_com/templates/urtps_bridge_topics.yaml
+
+python3 /root/PX4-Autopilot/msg/tools/uorb_to_ros_msgs.py \
+    /root/PX4-Autopilot/msg/ \
+    /root/px4_ros/src/px4_msgs/msg
+
+colcon build --build-base ~/px4_ros/build \
+    --install-base ~/px4_ros/install \
+    --base-paths ~/px4_ros/src \
+    --symlink-install \
+    --event-handlers console_direct+
