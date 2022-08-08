af4240197076878a5cc5c37686f9a519b48a3685
kestrel <samsani@naver.com>
2022-08-08 22:24:07 +0900

    Working on script

--- scripts/buildRos2Pkg.sh
+++ scripts/buildRos2Pkg.sh
@@ -5,26 +5,25 @@ rosWorksapce=/root/ros_ws
 colcon build \
   --build-base ${rosWorksapce}build \
   --install-base ${rosWorksapce}install \
-  --lob-base ${rosWorksapce}log \
   --base-paths ${rosWorksapce}src \
   --event-handlers console_direct+
 
-entry="${entry%/}"
-echo ${entry##*/}
+# entry="${entry%/}"
+# echo ${entry##*/}
 
-  if [ "${SHELL}" = "/usr/bin/zsh" ]; then
-    echo "ZSH Detected"
-    echo "source ${entry}/install/install.sh" >> /root/.zshrc
-  elif [ "${SHELL}" = "/usr/bin/bash" ]; then
-    echo "BASH Detected"
-    echo "source ${entry}/install/install.sh" >> /root/.bashrc
-  else
-    echo "Unidentified shell! Exiing script."
-    exit 1
-  fi
+#   if [ "${SHELL}" = "/usr/bin/zsh" ]; then
+#     echo "ZSH Detected"
+#     echo "source ${entry}/install/install.sh" >> /root/.zshrc
+#   elif [ "${SHELL}" = "/usr/bin/bash" ]; then
+#     echo "BASH Detected"
+#     echo "source ${entry}/install/install.sh" >> /root/.bashrc
+#   else
+#     echo "Unidentified shell! Exiing script."
+#     exit 1
+#   fi
 
-for entry in "${rosWorksapce}/src/*/"
-do
-  entry="${entry%/}"    
-  echo "ROS2 package ${entry##*/} processed!"
-done
+# for entry in "${rosWorksapce}/src/*/"
+# do
+#   entry="${entry%/}"    
+#   echo "ROS2 package ${entry##*/} processed!"
+# done
